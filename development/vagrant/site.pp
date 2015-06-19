2e8cf2101e66fcc2328e0ba2e1eb473f1c468610
Bernd Ahlers <bernd@graylog.com>
2015-06-19 14:36:06 +0200

    Update development env for 1.1.

--- development/vagrant/site.pp
+++ development/vagrant/site.pp
@@ -29,9 +29,12 @@ node default {
   class { 'graylog2::repo': version => '1.1' }
 
   class {'graylog2::server':
+    package_version            => '1.1.3-1',
     password_secret            => '16BKgz0Qelg6eFeJYh8lc4hWU1jJJmAgHlPEx6qkBa2cQQTUG300FYlPOEvXsOV4smzRtnwjHAKykE3NIWXbpL7yGLN7V2P2',
     root_password_sha2         => '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',
     elasticsearch_cluster_name => $es_cluster_name,
+    versionchecks              => false,
+    usage_statistics_enabled   => false,
     require                    => [
       Elasticsearch::Instance['esgl2'],
       Class['mongodb::server'],
@@ -40,6 +43,7 @@ node default {
   }
 
   class {'graylog2::web':
+    package_version    => '1.1.3-1',
     application_secret => '16BKgz0Qelg6eFeJYh8lc4hWU1jJJmAgHlPEx6qkBa2cQQTUG300FYlPOEvXsOV4smzRtnwjHAKykE3NIWXbpL7yGLN7V2P2',
     require            => Class['graylog2::server'],
     timeout            => '60s',
