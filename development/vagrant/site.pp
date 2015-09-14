e9a97f0e94669459dca0dfe68987ff9e560e2a51
Bernd Ahlers <bernd@graylog.com>
2015-09-14 18:03:38 +0200

    Bump versions to 1.2 in development site.pp

--- development/vagrant/site.pp
+++ development/vagrant/site.pp
@@ -26,10 +26,10 @@ node default {
 
 
   # Graylog2
-  class { 'graylog2::repo': version => '1.1' }
+  class { 'graylog2::repo': version => '1.2' }
 
   class {'graylog2::server':
-    package_version            => '1.1.3-1',
+    package_version            => '1.2.0-5',
     password_secret            => '16BKgz0Qelg6eFeJYh8lc4hWU1jJJmAgHlPEx6qkBa2cQQTUG300FYlPOEvXsOV4smzRtnwjHAKykE3NIWXbpL7yGLN7V2P2',
     root_password_sha2         => '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',
     elasticsearch_cluster_name => $es_cluster_name,
@@ -43,7 +43,7 @@ node default {
   }
 
   class {'graylog2::web':
-    package_version    => '1.1.3-1',
+    package_version    => '1.2.0-5',
     application_secret => '16BKgz0Qelg6eFeJYh8lc4hWU1jJJmAgHlPEx6qkBa2cQQTUG300FYlPOEvXsOV4smzRtnwjHAKykE3NIWXbpL7yGLN7V2P2',
     require            => Class['graylog2::server'],
     timeout            => '60s',
